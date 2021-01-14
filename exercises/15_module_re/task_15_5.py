6d78fba04c33b55b0b04933a065c22850974683e
left769 <berezavitaliy769@gmail.com>
2021-01-14 12:56:44 +0200

    exercise 15_5

--- exercises/15_module_re/task_15_5.py
+++ exercises/15_module_re/task_15_5.py
@@ -5,7 +5,8 @@
 Создать функцию generate_description_from_cdp, которая ожидает как аргумент
 имя файла, в котором находится вывод команды show cdp neighbors.
 
-Функция должна обрабатывать вывод команды show cdp neighbors и генерировать на основании вывода команды описание для интерфейсов.
+Функция должна обрабатывать вывод команды show cdp neighbors и генерировать на основании вывода команды описание
+для интерфейсов.
 
 Например, если у R1 такой вывод команды:
 R1>show cdp neighbors
@@ -18,9 +19,26 @@ SW1              Eth 0/0           140          S I      WS-C3750-  Eth 0/1
 Для интерфейса Eth 0/0 надо сгенерировать такое описание
 description Connected to SW1 port Eth 0/1
 
-Функция должна возвращать словарь, в котором ключи - имена интерфейсов, а значения - команда задающая описание интерфейса:
+Функция должна возвращать словарь, в котором ключи - имена интерфейсов, а значения - команда задающая
+описание интерфейса:
 'Eth 0/0': 'description Connected to SW1 port Eth 0/1'
 
 
 Проверить работу функции на файле sh_cdp_n_sw1.txt.
 """
+import re
+
+
+def generate_description_from_cdp(src_file):
+    result = {}
+    regex = (r'(\S+) +(\S+ \S+) +\d+ .+ +\S+ +(\S+ .+)\n')
+    with open(src_file) as f:
+        for line in f:
+            match = re.search(regex, line)
+            if match:
+                result[match.group(2)] = (f'description Connected to {match.group(1)} port {match.group(3)}')
+    return result
+
+
+if __name__ == "__main__":
+    print(generate_description_from_cdp('sh_cdp_n_sw1.txt'))
