a2aa42f85236bd640a38f2074921881bb654fa80
left769 <berezavitaliy769@gmail.com>
2021-02-21 11:27:39 +0200

    correction_exercise_21_1a

--- exercises/21_textfsm/task_21_1a.py
+++ exercises/21_textfsm/task_21_1a.py
@@ -15,16 +15,13 @@
 Проверить работу функции на выводе команды output/sh_ip_int_br.txt и шаблоне templates/sh_ip_int_br.template.
 """
 import textfsm
-from pprint import pprint
 
 
 def parse_output_to_dict(template, command_output):
     result = []
     with open(template) as temp:
         fsm = textfsm.TextFSM(temp)
-    with open(command_output) as f:
-        command = f.read()
-    output = fsm.ParseText(command)
+    output = fsm.ParseText(command_output)
     d_keys = fsm.header
     for d_values in output:
         result.append(dict(zip(d_keys, d_values)))
@@ -32,4 +29,6 @@ def parse_output_to_dict(template, command_output):
 
 
 if __name__ == "__main__":
-    pprint(parse_output_to_dict('templates/sh_ip_int_br.template', 'output/sh_ip_int_br.txt'))
+    with open('output/sh_ip_int_br.txt') as f:
+        command = f.read()
+    print(parse_output_to_dict('templates/sh_ip_int_br.template', command))
