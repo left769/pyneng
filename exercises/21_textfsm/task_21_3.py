87e8eab505cc6b76b5643b60045907820a74ea51
left769 <berezavitaliy769@gmail.com>
2021-02-26 12:32:03 +0200

    correction exercise_21_3

--- exercises/21_textfsm/task_21_3.py
+++ exercises/21_textfsm/task_21_3.py
@@ -22,13 +22,16 @@ from textfsm import clitable
 
 
 def parse_command_dynamic(command_output, attributes_dict, index_file='index', templ_path='templates'):
+    result = []
     cli_table = clitable.CliTable(index_file, templ_path)
     cli_table.ParseCmd(command_output, attributes_dict)
-    result = cli_table.FormattedTable()
+    headers = cli_table.header
+    for d_values in cli_table:
+        result.append(dict(zip(headers, d_values)))
     return result
 
 
-if __name__ == '__man__':
+if __name__ == '__main__':
     with open('output/sh_ip_int_br.txt') as f:
         sh_out = f.read()
     attributes = {'Command': 'sh ip int br', 'Vendor': 'cisco_ios'}
