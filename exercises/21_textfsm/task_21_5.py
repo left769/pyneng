52d9b5f3cfecf2f7d8e0c7a571a9759afa8d28fa
left769 <berezavitaliy769@gmail.com>
2021-02-26 15:22:11 +0200

    exercise_21_5

--- exercises/21_textfsm/task_21_5.py
+++ exercises/21_textfsm/task_21_5.py
@@ -13,3 +13,22 @@
 
 Теста для этого задания нет.
 """
+from itertools import repeat
+from task_21_4 import send_and_parse_show_command
+from concurrent.futures import ThreadPoolExecutor
+import yaml
+
+
+def send_and_parse_command_parallel(devices, command, templates_path, index='index'):
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        result = executor.map(send_and_parse_show_command, devices, repeat(command), repeat(templates_path), repeat(index))
+    for row in result:
+        print(row)
+    return result
+
+
+if __name__ == "__main__":
+    with open('devices.yaml') as f:
+        dev_list = yaml.safe_load(f)
+    show_command = 'sh ip int br'
+    print(send_and_parse_command_parallel(dev_list, show_command, 'templates'))
