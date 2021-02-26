1ec86d3edd196f430a9ab11da4a9de34af9c64b5
left769 <berezavitaliy769@gmail.com>
2021-02-26 14:32:47 +0200

    exercise_21_4

--- exercises/21_textfsm/task_21_4.py
+++ exercises/21_textfsm/task_21_4.py
@@ -19,3 +19,25 @@
 
 Проверить работу функции на примере вывода команды sh ip int br и устройствах из devices.yaml.
 """
+from task_21_3 import parse_command_dynamic
+from netmiko import ConnectHandler
+
+
+def send_and_parse_show_command(device_dict, command, templates_path, index='index'):
+    attributes = {'Command': command, 'Vendor': device_dict['device_type']}
+    ssh = ConnectHandler(**device_dict)
+    ssh.enable()
+    output = ssh.send_command(command)
+    result = parse_command_dynamic(output, attributes, index_file=index, templ_path=templates_path)
+    return result
+
+
+if __name__ == '__main__':
+    cisco_router = {
+        'device_type': 'cisco_ios',
+        'host': '192.168.100.93',
+        'username': 'cisco',
+        'password': 'cisco',
+        'secret': 'cisco'
+    }
+    print(send_and_parse_show_command(cisco_router, 'sh ip int br', 'templates'))
