347902de90035e057ad38bcaddc387d2be6d759a
left769 <berezavitaliy769@gmail.com>
2021-03-11 14:10:58 +0200

    exercise_22_1a

--- exercises/22_oop_basics/task_22_1a.py
+++ exercises/22_oop_basics/task_22_1a.py
@@ -8,11 +8,18 @@
 Перенести функциональность удаления дублей в метод _normalize.
 При этом метод __init__ должен выглядеть таким образом:
 """
+from pprint import pprint
 
 
 class Topology:
     def __init__(self, topology_dict):
         self.topology = self._normalize(topology_dict)
+    def _normalize(self, topology_dict):
+        formatted_topology = {}
+        for local_device, rem_device in topology_dict.items():
+            if rem_device not in formatted_topology.keys():
+                formatted_topology[local_device] = rem_device
+        return formatted_topology
 
 
 topology_example = {
@@ -26,3 +33,8 @@ topology_example = {
     ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
+
+
+if __name__ == '__main__':
+    top = Topology(topology_example)
+    pprint(top.topology)
