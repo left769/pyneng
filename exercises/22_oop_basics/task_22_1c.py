f0fa0764579e56bd1bbb01df165db410706d7323
left769 <berezavitaliy769@gmail.com>
2021-03-12 10:12:00 +0200

    exercise_22_1c

--- exercises/22_oop_basics/task_22_1c.py
+++ exercises/22_oop_basics/task_22_1c.py
@@ -35,6 +35,7 @@ In [5]: t.delete_node('SW1')
 Такого устройства нет
 
 """
+from pprint import pprint
 
 topology_example = {
     ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
@@ -47,3 +48,40 @@ topology_example = {
     ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
+
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+
+    def _normalize(self, topology_dict):
+        formatted_topology = {}
+        for local_device, rem_device in topology_dict.items():
+            if rem_device not in formatted_topology.keys():
+                formatted_topology[local_device] = rem_device
+        return formatted_topology
+
+    def delete_link(self, src_dev, dst_dev):
+        result = self.topology
+        direct_link = result.get(src_dev)
+        reverse_link = result.get(dst_dev)
+        if direct_link == dst_dev:
+            del result[src_dev]
+        elif reverse_link == src_dev:
+            del result[dst_dev]
+        else:
+            print('Такого соединения нет')
+
+    def delete_node(self, device_name):
+        result = self.topology.copy()
+        for link in self.topology.items():
+            for peer in link:
+                if peer[0] == device_name:
+                    del result[link[0]]
+        if result != self.topology:
+            print('Такого устройства нет')
+
+
+if __name__ == '__main__':
+    top = Topology(topology_example)
+    pprint(top.delete_node('R'))
