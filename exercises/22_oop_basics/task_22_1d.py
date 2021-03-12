5e3fcddb2e2f6a2c52fa7407acee04e5a40d0221
left769 <berezavitaliy769@gmail.com>
2021-03-12 11:42:56 +0200

    exercise_22_1d

--- exercises/22_oop_basics/task_22_1d.py
+++ exercises/22_oop_basics/task_22_1d.py
@@ -42,6 +42,7 @@ Cоединение с одним из портов существует
 
 
 """
+from pprint import pprint
 
 topology_example = {
     ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
@@ -54,3 +55,53 @@ topology_example = {
     ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
+
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+
+    def _normalize(self, topology_dict):
+        formatted_topology = {}
+        for local_device, rem_device in topology_dict.items():
+            if rem_device not in formatted_topology.keys():
+                formatted_topology[local_device] = rem_device
+        return formatted_topology
+
+    def delete_link(self, src_dev, dst_dev):
+        result = self.topology
+        direct_link = result.get(src_dev)
+        reverse_link = result.get(dst_dev)
+        if direct_link == dst_dev:
+            del result[src_dev]
+        elif reverse_link == src_dev:
+            del result[dst_dev]
+        else:
+            print('Такого соединения нет')
+
+    def delete_node(self, device_name):
+        result = self.topology.copy()
+        for link in result.items():
+            for peer in link:
+                if peer[0] == device_name:
+                    del self.topology[link[0]]
+        if result == self.topology:
+            print('Такого устройства нет')
+
+    def add_link(self, src_dev, dst_dev):
+        for link in self.topology.items():
+            if link == (src_dev, dst_dev):
+                print('Такое соединение существует')
+                return None
+        if src_dev in self.topology.keys() or src_dev in self.topology.values():
+            print('Cоединение с одним из портов существует')
+        elif dst_dev in self.topology.keys() or dst_dev in self.topology.values():
+            print('Cоединение с одним из портов существует')
+        else:
+            self.topology[src_dev] = dst_dev
+
+
+if __name__ == '__main__':
+    top = Topology(topology_example)
+    pprint(top.add_link(("R1", "Eth0/0"), ("SW1", "Eth0/1")))
+    pprint(top.topology)
