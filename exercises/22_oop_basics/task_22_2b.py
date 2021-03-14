5d995f7db6f8b32221eeb5cdf08f2163228f766b
left769 <berezavitaliy769@gmail.com>
2021-03-14 15:26:32 +0200

    exercise_22_2b

--- exercises/22_oop_basics/task_22_2b.py
+++ exercises/22_oop_basics/task_22_2b.py
@@ -29,3 +29,81 @@ In [6]: r1.send_config_commands(['interface loop55', 'ip address 5.5.5.5 255.255
 Out[6]: 'conf t\r\nEnter configuration commands, one per line.  End with CNTL/Z.\r\nR1(config)#interface loop55\r\nR1(config-if)#ip address 5.5.5.5 255.255.255.255\r\nR1(config-if)#end\r\nR1#'
 
 """
+import telnetlib
+import time
+from useful_scripts import parse_command_dynamic
+
+
+class CiscoTelnet:
+    def __init__(self, ip, username, password, secret):
+        self.ip = ip
+        self.username = username
+        self.password = password
+        self. secret = secret
+
+    def _write_line(self, word):
+        return (word + '\n').encode('utf-8')
+
+    def send_show_command(self, show_command, parse=True, index='index', templates='templates'):
+        with telnetlib.Telnet(self.ip) as telnet:
+            telnet.read_until(b"Username")
+            telnet.write(self._write_line(self.username))
+            telnet.read_until(b"Password")
+            telnet.write(self._write_line(self.password))
+            index, m, output = telnet.expect([b">", b"#"])
+            if index == 0:
+                telnet.write(b"enable\n")
+                telnet.read_until(b"Password")
+                telnet.write(self._write_line(self.secret))
+                telnet.read_until(b"#", timeout=5)
+                time.sleep(3)
+                telnet.read_very_eager()
+
+            telnet.write(self._write_line(show_command))
+            out = telnet.read_until(b"#", timeout=5).decode("utf-8")
+        if parse:
+            connection_info = {'Command': show_command, 'Vendor': 'cisco_ios'}
+            return parse_command_dynamic(out, connection_info, 'index', templates)
+        else:
+            return out
+
+    def send_config_commands(self, config_command):
+        with telnetlib.Telnet(self.ip) as telnet:
+            telnet.read_until(b"Username")
+            telnet.write(self._write_line(self.username))
+            telnet.read_until(b"Password")
+            telnet.write(self._write_line(self.password))
+            index, m, output = telnet.expect([b">", b"#"])
+            if index == 0:
+                telnet.write(b"enable\n")
+                telnet.read_until(b"Password")
+                telnet.write(self._write_line(self.secret))
+                telnet.read_until(b"#", timeout=5)
+            if type(config_command) == str:
+                telnet.write(self._write_line('conf t'))
+                conf_t = telnet.read_until(b"(config)#")
+                telnet.write(self._write_line(config_command))
+                out = telnet.read_until(b"(config)#")
+                telnet.write(self._write_line('end'))
+                the_end = telnet.read_until(b"#")
+                result = conf_t.decode('utf-8') + out.decode('utf-8') + the_end.decode('utf-8')
+            else:
+                telnet.write(self._write_line('conf t'))
+                result = telnet.read_until(b"(config)#").decode('utf-8')
+                for command in config_command:
+                    telnet.write(self._write_line(command))
+                    out = telnet.read_until(b"#")
+                    result = result + out.decode('utf-8')
+                telnet.write(self._write_line('end'))
+                result = result + telnet.read_until(b"#").decode('utf-8')
+        return result
+
+
+if __name__ == '__main__':
+    r1_params = {
+        'ip': '192.168.100.1',
+        'username': 'cisco',
+        'password': 'cisco',
+        'secret': 'cisco'}
+    test = CiscoTelnet(**r1_params)
+    print(test.send_config_commands(['interface loop55', 'ip address 5.5.5.5 255.255.255.255']))
