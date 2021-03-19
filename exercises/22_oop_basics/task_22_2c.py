c085c9ec510e77036e2252da19ebfa521d32d736
left769 <berezavitaliy769@gmail.com>
2021-03-19 15:47:33 +0200

    test_exercise_22_2c

--- exercises/22_oop_basics/task_22_2c.py
+++ exercises/22_oop_basics/task_22_2c.py
@@ -60,3 +60,82 @@ ValueError                                Traceback (most recent call last)
 ValueError: При выполнении команды "logging 0255.255.1" на устройстве 192.168.100.1 возникла ошибка -> Invalid input detected at '^' marker.
 
 """
+import telnetlib
+import time
+from useful_scripts import parse_command_dynamic
+
+
+class CiscoTelnet:
+    def __init__(self, ip, username, password, secret):
+        self.ip = ip
+        self.username = username
+        self.password = password
+        self.secret = secret
+        self.telnet = telnetlib.Telnet(ip)
+        self.telnet.read_until(b"Username:")
+        self._write_line(username)
+        self.telnet.read_until(b"Password:")
+        self._write_line(password)
+        self._write_line("enable")
+        self.telnet.read_until(b"Password:")
+        self._write_line(secret)
+        self._write_line("terminal length 0")
+        time.sleep(1)
+        self.telnet.read_very_eager()
+
+    def _write_line(self, word):
+        return (word + '\n').encode('utf-8')
+
+    def send_show_command(self, show_command, parse=True, index='index', templates='templates'):
+        with telnetlib.Telnet(self.ip) as telnet:
+            telnet.read_until(b"Username")
+            telnet.write(self._write_line(self.username))
+            telnet.read_until(b"Password")
+            telnet.write(self._write_line(self.password))
+            index, m, output = telnet.expect([b">", b"#"])
+            if index == 0:
+                telnet.write(b"enable\n")
+                telnet.read_until(b"Password")
+                telnet.write(self._write_line(self.secret))
+                telnet.read_until(b"#", timeout=5)
+                time.sleep(3)
+                telnet.read_very_eager()
+
+            telnet.write(self._write_line(show_command))
+            out = telnet.read_until(b"#", timeout=5).decode("utf-8")
+        if parse:
+            connection_info = {'Command': show_command, 'Vendor': 'cisco_ios'}
+            return parse_command_dynamic(out, connection_info, 'index', templates)
+        else:
+            return out
+
+    def send_config_commands(self, config_command, strict=True):
+        result = ''
+        error_flow = ''
+        if type(config_command) == str:
+            config_command = [config_command]
+        self._write_line("conf t")
+
+        for command in config_command:
+            self._write_line(command)
+            time.sleep(1)
+            out = self.telnet.read_very_eager().decode('utf-8')
+            result = result + out
+            self._write_line('end')
+            result = result + self.telnet.read_very_eager().decode('utf-8')
+        return result
+
+
+
+
+if __name__ == '__main__':
+    commands_with_errors = ['logging 0255.255.1', 'logging', 'a']
+    correct_commands = ['logging buffered 20010', 'ip http server']
+    commands = commands_with_errors + correct_commands
+    r1_params = {
+        'ip': '192.168.100.1',
+        'username': 'cisco',
+        'password': 'cisco',
+        'secret': 'cisco'}
+    test = CiscoTelnet(**r1_params)
+    print(test.send_config_commands(commands, strict=False))
