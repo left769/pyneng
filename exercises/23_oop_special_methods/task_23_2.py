46d45365171e8c78f98e97eecf0abeb15b1281bb
left769 <berezavitaliy769@gmail.com>
2021-03-23 18:45:34 +0200

    exercise_23_2

--- exercises/23_oop_special_methods/task_23_2.py
+++ exercises/23_oop_special_methods/task_23_2.py
@@ -40,3 +40,81 @@ ValueError                                Traceback (most recent call last)
 
 ValueError: Возникла ошибка
 """
+import telnetlib
+import time
+import re
+from useful_scripts import parse_command_dynamic
+
+
+class CiscoTelnet:
+    def __init__(self, ip, username, password, secret):
+        self.ip = ip
+        self.username = username
+        self.password = password
+        self.secret = secret
+        self.telnet = telnetlib.Telnet(ip)
+        self.telnet.read_until(b"Username")
+        self._write_line(username)
+        self.telnet.read_until(b"Password")
+        self._write_line(password)
+        self._write_line('enable')
+        self.telnet.read_until(b"Password:")
+        self._write_line(secret)
+        self._write_line("terminal length 0")
+        time.sleep(1)
+        self.telnet.read_very_eager()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.telnet.close()
+
+    def _write_line(self, word):
+        self.telnet.write((word + '\n').encode('utf-8'))
+
+    def send_show_command(self, show_command, parse=False, templates='templates'):
+        self._write_line(show_command)
+        result = self.telnet.read_until(b"#").decode("utf-8")
+        time.sleep(1)
+        if parse:
+            connection_info = {'Command': show_command, 'Vendor': 'cisco_ios'}
+            return parse_command_dynamic(result, connection_info, 'index', templates)
+        else:
+            return result
+
+    def send_config_commands(self, config_command, strict=True):
+        result = ''
+        if type(config_command) == str:
+            config_command = [config_command]
+
+        self._write_line('conf t')
+        for command in config_command:
+            self._write_line(command)
+            time.sleep(1)
+            out = self.telnet.read_very_eager().decode("utf-8")
+            error_check = re.search(r'% (?P<error>.+)', out)
+            if error_check and strict:
+                raise ValueError(f'При выполнении команды "{command}" на устройстве {self.ip} '
+                                 f'возникла ошибка -> {error_check.group(1)}')
+            elif error_check and not strict:
+                print(f'При выполнении команды "{command}" на устройстве {self.ip} '
+                      f'возникла ошибка -> {error_check.group(1)}')
+            result = result + out
+
+        self._write_line("end")
+        time.sleep(1)
+        out = self.telnet.read_very_eager().decode("utf-8")
+        result = result + out
+        return result
+
+
+if __name__ == '__main__':
+    r1_params = {
+        'ip': '192.168.100.1',
+        'username': 'cisco',
+        'password': 'cisco',
+        'secret': 'cisco'}
+    with CiscoTelnet(**r1_params) as r1:
+        print(r1.send_show_command('sh clock'))
+        raise ValueError('Возникла ошибка')
