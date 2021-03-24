9e3f9820058a430d73aebdbb71d22dcb1b9b42af
left769 <berezavitaliy769@gmail.com>
2021-03-24 17:50:07 +0200

    exercise_24_1 initial commit

--- exercises/24_oop_inheritance/task_24_1.py
+++ exercises/24_oop_inheritance/task_24_1.py
@@ -17,6 +17,7 @@ In [4]: r1.send_show_command('sh ip int br')
 Out[4]: 'Interface                  IP-Address      OK? Method Status                Protocol\nEthernet0/0                192.168.100.1   YES NVRAM  up                    up      \nEthernet0/1                192.168.200.1   YES NVRAM  up                    up      \nEthernet0/2                190.16.200.1    YES NVRAM  up                    up      \nEthernet0/3                192.168.230.1   YES NVRAM  up                    up      \nEthernet0/3.100            10.100.0.1      YES NVRAM  up                    up      \nEthernet0/3.200            10.200.0.1      YES NVRAM  up                    up      \nEthernet0/3.300            10.30.0.1       YES NVRAM  up                    up      '
 
 """
+from base_connect_class import BaseSSH
 
 device_params = {
     "device_type": "cisco_ios",
@@ -25,3 +26,9 @@ device_params = {
     "password": "cisco",
     "secret": "cisco",
 }
+
+
+class CiscoSSH(BaseSSH):
+    def __init__(self, ip, username, password, enable_password):
+        super.__init__(ip, username, password, enable_password)
+        self.ssh.enable()
