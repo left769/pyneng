81d28b453f911955c49c0210edba01372e0c6096
left769 <berezavitaliy769@gmail.com>
2021-03-26 09:36:16 +0200

    exercise_25_2

--- exercises/25_db/task_25_2/get_data.py
+++ exercises/25_db/task_25_2/get_data.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 import sqlite3
 from tabulate import tabulate
+from sys import argv
 
 
 def get_info(parameter=None, value=None):
     connection = sqlite3.connect('dhcp_snooping.db')
     cursor = connection.cursor()
     if parameter and value:
-        query = f'SELECT * FROM dhcp WHERE {parameter} = ?'
-        result = cursor.execute(query, (value,))
-        return result.fetchall()
+        try:
+            query = f'SELECT * FROM dhcp WHERE {parameter} = ?'
+            result = cursor.execute(query, (value,))
+            return result.fetchall()
+        except sqlite3.OperationalError:
+            return 'Данный параметр не поддерживается.\n' \
+                   'Допустимые значения параметров: mac, ip, vlan, interface, switch'
     elif not parameter and not value:
         query = 'SELECT * FROM dhcp'
         result = cursor.execute(query)
@@ -19,6 +24,9 @@ def get_info(parameter=None, value=None):
 
 
 if __name__ == '__main__':
-    keys = ['MAC', 'IP', 'VLAN', 'INTERFACE', 'SWITCH']
-    local_result = get_info()
-    print(tabulate(local_result, headers=keys, tablefmt="grid", stralign='center'))
+    local_result = get_info(argv[1], argv[2])
+    if isinstance(local_result, list):
+        keys = ['MAC', 'IP', 'VLAN', 'INTERFACE', 'SWITCH']
+        print(tabulate(local_result, headers=keys, tablefmt="grid", stralign='center'))
+    else:
+        print(local_result)
